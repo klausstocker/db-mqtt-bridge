dbd4c4e59912f38f3d71d62283717909e03f74bb
Klaus Stocker <klaus.stocker@gmx.net>
2021-09-25 21:56:59 +0200

    CHG adapt to current database

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from config import config_db
 import pymysql as mariadb
 import threading
 import time
-import datetime
+from datetime import datetime
 
 print(config_db)
 
@@ -10,7 +10,7 @@ CONNECTION_CHECK_INTERVAL_SECONDS = 2
 
 
 def timestamp():
-    now = datetime.datetime.now()  # current date and time
+    now = datetime.now()  # current date and time
     return now.strftime("%m/%d/%Y, %H:%M:%S ")
 
 
@@ -80,11 +80,59 @@ class Db:
                 self.__db.commit()  # we need commit() to be update fom db
                 result = cur.fetchall()
                 return result
-        except:
-            print(timestamp() + "Database query error")
+        except Exception as e:
+            print(timestamp() + "Database query error: " + str(e))
             return tuple()
         finally:
             pass
 
+    def _deny(self):
+        return {"access": "no"}
 
-db = Db()
+    def checkRfid(self, id, address):
+        # -------------------------------------------------
+        # database request
+        # -------------------------------------------------
+        result = db.query(sql='SELECT device_id, autoOff FROM devices WHERE reader = %s', param=(address,))
+        # ------------------------------------------------
+        # handling the reply
+        # -------------------------------------------------
+        if result == tuple():
+            print("No device for this address: " + address)
+            return self._deny()
+        
+        if len(result) != 1:
+            print("address (%s) is not unique, check database: " % address)
+            return self._deny()
+
+        deviceId = int(result[0]['device_id'])
+        autoOff = result[0]['autoOff']
+        sql = """SELECT users.rfid, rights.device_id, users.user_id, users.until, userCategories.restriction 
+                FROM users INNER JOIN rights ON users.user_id = rights.user_id INNER JOIN devices ON 
+                rights.device_id = devices.device_id INNER JOIN userCategories ON 
+                users.userCategory = userCategories.id 
+                WHERE users.rfid = %s AND rights.device_id = %s AND devices.maintenance = 0"""
+        
+        result = db.query(sql=sql, param=(id, deviceId))
+        
+        if len(result) != 1:
+            return self._deny()
+        
+        until = result[0]['until']
+        if datetime.now().date() > until:
+            print('rfid %s has expired' % rfid)
+            return self._deny()
+            
+        restriction = result[0]['restriction']
+        if not eval(restriction):
+            print('rfid %s is not allowed at this time' % id)
+            return self._deny()
+ 
+        return {"access": "yes", "value": int(autoOff), "unit": "s"}
+
+
+if __name__ == '__main__':
+    db = Db()
+    print(db.checkRfid('4DAB4ED3', '192.168.234.11'))
+    print(db.checkRfid('D64E381A', '192.168.234.11'))
+    
