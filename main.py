6425abbba0c31faff32807307162929da4133bbe
Franz Krenn <fkrenn@nanet.at>
2021-09-26 12:45:55 +0200

    corrected __main__ into __name__ , topic scheme improved

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 from mqtt import mqtt
-from db import db
+import db as database
 import threading
-import json
 
-# on_message is called from mqtt client
-# therefore this function runs under the client thread
-# and sometimes you would need a lock
-
-lock = threading.Lock()
+db = database.Db()
 
 
 def on_message(client, userdata, msg):
@@ -16,55 +11,20 @@ def on_message(client, userdata, msg):
         # doing stuff with message
         # -------------------------------------------------
         print("INCOMING Message: ", msg.topic, msg.payload)
-
+        id = msg.topic.split("/")[-1]
+        result = db.checkRfid(id, 'dummy_address')
         # -------------------------------------------------
-        # Der esp32 sendet folgenden topic, payload Inhalt
-        # -------------------------------------------------
-        # TOPIC                             PAYLOAD
-        # mksp/request/MAC-ADRESSE          chip id
-        # Ein komkretes Beispiel:
-        # TOPIC                             PAYLOAD
-        # mksp/request/80:7d:3a:0f:fa:65    59d71399
-        # -------------------------------------------------
-
-        # -------------------------------------------------
-        # database request
-        # -------------------------------------------------
-        mac = msg.topic.split("/")[-1]
-        irfd = msg.payload.decode("utf-8")
-
-        # Dieser sql string ist natürlich nur ein Beispiel
-
-        result = db.query(sql="SELECT name FROM makerspace.user WHERE mac=%s AND irfd=%s", param=(mac, irfd))
-
-        # simulierter result
-        result = {"access": "yes", "value": 5, "unit": "min"}
-
-        # -------------------------------------------------
-        # handling the reply
-        # -------------------------------------------------
-        if result == tuple():
-            print("Rejected id: " + irfd)
-            result = dict()  # empty dict
-        else:
-            print(result)
-
-        # --------------------------------
         # send mqtt message
+        mqtt.publish(topic="mksp/master/reply/" + id, payload=str(result).encode("utf-8"), retain=False)
         # -------------------------------------------------
-
-        # -------------------------------------------------
-        # Ergebnis zurück an den Sender
-        # -------------------------------------------------
-        # TOPIC                             PAYLOAD
-        # mksp/reply/MAC-ADRESSE            access-dict
-        # Ein komkretes Beispiel:
-        # TOPIC                             PAYLOAD
-        # mksp/reply/80:7d:3a:0f:fa:65      {"access": "yes", "value": 5, "unit": "min"}
-        # -------------------------------------------------
-        mqtt.publish(topic="mksp/reply/" + mac, payload=json.dumps(result).encode("utf-8"), retain=False)
     except Exception as e:
         print(e.__str__())
 
 
-mqtt.on_message = on_message
+if __name__ == '__main__':
+    # on_message is called from mqtt client
+    # therefore this function runs under the client thread
+    # and sometimes you would need a lock
+
+    lock = threading.Lock()
+    mqtt.on_message = on_message
